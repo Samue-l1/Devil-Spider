953b100eeccfc080175c55a65a29b498d6be0a17
King Sam <141226873+Samue-l1@users.noreply.github.com>
2024-06-28 14:10:59 +0300

    Update config.js

--- config.js
+++ config.js
@@ -23,8 +23,8 @@ global.location = '1' //Ganti Pake Location Panel Lu
 //===========================//
 
 global.prefa = ['','!','.',',','🐤','🗿']
-global.owner = ['254104301695']
-global.ownMain = '254104301695'
+global.owner = ['254742491666']
+global.ownMain = '254742491666'
 global.NamaOwner = '𝕶𝖎𝖓𝖌 𝕾𝖆𝖒' //gausah diganti 
 global.sessionName = 'spidersession'
 global.connect = true // Ubah Ke False Jika Ingin Menggunakan Qr Code
